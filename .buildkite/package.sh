9e69734dad4a761e8109571ad394bc178d4abed0
Kim Altintop <kim@monadic.xyz>
2019-09-03 10:45:59 +0200

    Publish .deb package to bintray

---
+++ .buildkite/package.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+version="$(cargo read-manifest|jq -r .version)+${BUILDKITE_BUILD_NUMBER}"
+deb="zockervols_${version}_amd64.deb"
+
+cargo deb --deb-version="${version}"
+
+echo "Uploading ${deb} to bintray:"
+curl -sSf \
+    -T "target/debian/${deb}"  \
+    -u"kalt:${BINTRAY_API_KEY}" \
+    -H"X-Bintray-Debian-Distribution: buster" \
+    -H"X-Bintray-Debian-Component: main" \
+    -H"X-Bintray-Debian-Architecture: amd64" \
+    "https://api.bintray.com/content/oscoin/zockervols/zockervols/${version}/${deb}"
+echo
+
+echo "Publishing ${deb} on bintray:"
+curl -sSf -XPOST \
+    -u"kalt:${BINTRAY_API_KEY}" \
+   "https://api.bintray.com/content/oscoin/zockervols/zockervols/${version}/publish"
+echo
