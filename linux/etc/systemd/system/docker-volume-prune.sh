4effb13c0ece9579d92b1eb7ed6471c1dbd93832
Kim Altintop <kim.altintop@gmail.com>
2019-11-25 21:31:21 +0100

    Fix volume prune script: `read -ra`

--- linux/etc/systemd/system/docker-volume-prune.sh
+++ linux/etc/systemd/system/docker-volume-prune.sh
@@ -3,15 +3,14 @@ set -eou pipefail
 
 yesterday="$(date --utc --date='yesterday' +'%Y-%m-%dT00:00:00Z')"
 
-declare -a prune
-prune=("$(
+read -ra prune <<< "$(
     docker volume ls --filter=label=build_cache --format='{{ .Name }}' \
     | xargs --no-run-if-empty docker volume inspect \
     | jq -rM --arg date "$yesterday" \
         '. | map(select(.Mountpoint == "none")) | map(select(.CreatedAt < $date)) | map(.Name) | .[]'
-)")
+)"
 
-if [ "${prune[0]}" ]
+if [ ${#prune[@]} -gt 0 ]
 then
     set -x
     for vol in "${prune[@]}"
