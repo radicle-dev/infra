8ee035e5eab05f939032bab920066b0cedc5e4fb
Kim Altintop <kim@monadic.xyz>
2019-12-20 15:15:53 +0100

    Use mapfile instead of read -ra when evaluating prunable cache volumes.
    shellcheck did recommend read at some point, but that doesn't seem to do
    the right thing.
    
    Fixes #30

--- linux/etc/systemd/system/docker-volume-prune.sh
+++ linux/etc/systemd/system/docker-volume-prune.sh
@@ -3,19 +3,21 @@ set -eou pipefail
 
 yesterday="$(date --utc --date='yesterday' +'%Y-%m-%dT00:00:00Z')"
 
-read -ra prune <<< "$(
+mapfile -t prune < <(
     docker volume ls --filter=label=build_cache --format='{{ .Name }}' \
     | xargs --no-run-if-empty docker volume inspect \
     | jq -rM --arg date "$yesterday" \
         '. | map(select(.Mountpoint == "none")) | map(select(.CreatedAt < $date)) | map(.Name) | .[]'
-)"
+)
 
 if [ ${#prune[@]} -gt 0 ]
 then
-    set -x
+    set +e
     for vol in "${prune[@]}"
     do
+        set -x
         docker volume rm "$vol"
+        set +x
     done
 else
     echo "No prunable docker volumes found."
