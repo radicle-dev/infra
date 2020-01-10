b51eb5e03cc258391a6905e2a4d8b0bfa0a5b1f8
Kim Altintop <kim@monadic.xyz>
2020-01-10 23:41:00 +0100

    Build script

--- .buildkite/build-buildkite-hooks.sh
+++ .buildkite/build-buildkite-hooks.sh
@@ -6,12 +6,20 @@ pushd buildkite-hooks
 version="$(cargo read-manifest|jq -r .version)+${BUILDKITE_BUILD_NUMBER}"
 deb="buildkite-hooks_${version}_amd64.deb"
 
+echo "--- cargo test"
+cargo test --all
+
+echo "--- scripted tests"
+test/test-cmd-signal
+test/test-cmd-timeout
+
+echo "--- cargo deb"
 cargo deb --deb-version="${version}"
 
 if [[ "$BUILDKITE_BRANCH" == "master" ]]
 then
-    echo "Uploading ${deb} to bintray:"
-    curl -sSf \
+    echo "--- Uploading ${deb} to bintray"
+    curl -f \
         -T "target/debian/${deb}"  \
         -u"${BINTRAY_API_KEY}" \
         -H"X-Bintray-Debian-Distribution: buster" \
@@ -20,8 +28,8 @@ then
         "https://api.bintray.com/content/oscoin/buildkite-hooks/buildkite-hooks/${version}/${deb}"
     echo
 
-    echo "Publishing ${deb} on bintray:"
-    curl -sSf -XPOST \
+    echo "--- Publishing ${deb} on bintray"
+    curl -f -XPOST \
         -u"${BINTRAY_API_KEY}" \
        "https://api.bintray.com/content/oscoin/buildkite-hooks/buildkite-hooks/${version}/publish"
     echo
