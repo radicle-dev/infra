1cb3db1d08a2f88fa8c30bd1ed0d285d4d85abb5
Thomas Scholtes <thomas@monadic.xyz>
2020-05-18 13:26:12 +0200

    Don’t use GCE credentials for chown command

--- ci/src/bin/buildkite-pre-checkout-hook.rs
+++ ci/src/bin/buildkite-pre-checkout-hook.rs
@@ -4,10 +4,7 @@ use failure::Error;
 use log::{debug, info};
 use paw;
 
-use buildkite_hooks::{
-    cmd::CommandExt,
-    config::{Config, GoogleApplicationCredentials},
-};
+use buildkite_hooks::{cmd::CommandExt, config::Config};
 
 #[paw::main]
 
@@ -55,15 +52,10 @@ fn decrypt_repo_secrets(cfg: &Config) -> Result<(), Error> {
 fn own_checkout_path(cfg: &Config) -> Result<(), Error> {
     info!("Adjusting checkout path ownership");
 
-    let mut chown = Command::sudo();
-
-    chown
+    Command::sudo()
         .args(&["chown", "-R", "buildkite-agent"])
-        .arg(&cfg.checkout_path);
-
-    if let GoogleApplicationCredentials::Json(path) = &cfg.google_application_credentials {
-        chown.env("GOOGLE_APPLICATION_CREDENTIALS", path);
-    }
-
-    chown.safe()?.succeed().map_err(|e| e.into())
+        .arg(&cfg.checkout_path)
+        .safe()?
+        .succeed()?;
+    Ok(())
 }
